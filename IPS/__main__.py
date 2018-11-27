e388970f34e4fd10e08470f7813dd69deef2c341
Bill Morrisson <billmorrissonjr@gmail.com>
2018-11-26 20:20:24 -0500

    Checking if the program is there

--- IPS/__main__.py
+++ IPS/__main__.py
@@ -14,3 +14,15 @@ def which(program):
     """ Tries to first determing if the program exists"""
     def is_exec(fpath):
         return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.name.split(program)
+    if fpath:
+        if is_exec(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exec_file = os.path.join(path, program)
+            if is_exec(exec_file):
+                return exec_file
+    raise
