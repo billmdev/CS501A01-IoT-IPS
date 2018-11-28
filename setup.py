534d1b0655804480495e709293cdbc10d334e27e
Bill Morrisson <billmorrissonjr@gmail.com>
2018-11-28 14:27:59 -0500

    Adding setup tools for the tool to be installed through pip

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='IPS',
+    packages=['IPS'],
+    version='',
+    description='A little tshark wrapper to count the number of cellphones in a particular limited area.',
+    author='ghostbill',
+    url='https://github.com/ghostbill/CS501A01-IoT-IPS',
+    author_email='billmorrissonjr@gmail.com'
+    keywords=['tshark', 'wifi', 'location'],
+    install_requires=[
+        "click",
+        "netifaces",
+        "pick",
+    ],
+    setup_requires=[],
+    tests_require=[],
+    entry_points={'console_scripts': [
+        'IPS = IPS.__main__:main',
+    ], },
+)
